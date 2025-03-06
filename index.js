69b722132c2722b13139844653e3caba8a2cf325
Thato <albanostlabo0@gmail.com>
2025-03-06 12:57:16 +0200

    feat: adding some backend

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const multer = require('multer');
+
+const port = process.env.PORT || 3000;
+const upload = multer({ dest: 'uploads/'})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if(!req.file){
+    return res.status(400).json({ error: 'No file uploaded'})
+  }
+
+  const metadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+
+  res.json(metadata)
+})
 
 
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
